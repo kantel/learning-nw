6325e1e66d2622b3aee670d1bab1d0a8091ba438
Jörg Kantel <joerg@kantel.de>
2019-11-27 19:11:30 +0100

    Reloaded 2

---
+++ nw-p5/hellop501/start.sh
@@ -0,0 +1,2 @@
+cd /Users/admin/git/learning-nw/nw-p5/hellop501
+/Applications/nwjs-sdk-v0.42.5-osx-x64/nwjs.app/Contents/MacOS/nwjs .
---
+++ nw-reload/nwmenue/start.sh
@@ -0,0 +1,2 @@
+cd /Users/admin/git/learning-nw/nw-reload/nwmenue
+/Applications/nwjs-sdk-v0.42.5-osx-x64/nwjs.app/Contents/MacOS/nwjs .
